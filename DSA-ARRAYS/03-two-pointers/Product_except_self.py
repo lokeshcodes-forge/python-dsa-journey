857adc9f716522260f0980366e6846fde25c21d4
lokeshcodes-forge <xx952535@gmail.com>
2026-09-18 23:02:23 +0530

    product expect self

---
+++ DSA-ARRAYS/03-two-pointers/Product_except_self.py
@@ -0,0 +1,20 @@
+nums = [1, 2, 3, 4]
+
+n = len(nums)
+answer = [1] * n
+
+# Prefix product
+prefix = 1
+
+for i in range(n):
+    answer[i] = prefix
+    prefix *= nums[i]
+
+# Suffix product
+suffix = 1
+
+for i in range(n - 1, -1, -1):
+    answer[i] *= suffix
+    suffix *= nums[i]
+
+print(answer)
