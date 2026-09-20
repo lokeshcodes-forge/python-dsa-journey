456d813931f81dc7015921548b3d5b2cea500ad8
lokeshcodes-forge <xx952535@gmail.com>
2026-09-20 23:00:01 +0530

    add monotonic

---
+++ numpy/indexing.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+a = np.array([[10,20,30],
+              [40,50,60,],
+              [70,80,90]])
+
+print(a[1:4])
+print(a[0,0])
+print(a[0:1])
+print(a[2:2])
+print(a[1:1:])
+print(a[:2,:2])
+print(a[::-1])
+print(a[:,::-1])
